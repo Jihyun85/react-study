a455947079e91ef4e92993305df7bda56063e38c
Jihyun <hwiyu25@gmail.com>
2021-03-28 17:31:23 +0900

    Create usePreventLeave

---
+++ src/usePreventLeave/usePreventLeave.js
@@ -0,0 +1,11 @@
+//usePreventLeave : 예를들면 저장하지 않고 떠날 때 안내문구
+export const usePreventLeave = () => {
+  const listener = (e) => {
+    e.preventDefault();
+    e.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () =>
+    window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
+};
