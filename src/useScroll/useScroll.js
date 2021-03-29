7d97b61939dabbbcbde74e93c3a4d9824d19ddb4
Jihyun <hwiyu25@gmail.com>
2021-03-29 16:49:52 +0900

    Create use Notification

--- src/useScroll/useScroll.js
+++ src/useScroll/useScroll.js
@@ -1,20 +1,20 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 export const useScroll = () => {
   const [state, setState] = useState({
     x: 0,
     y: 0,
-  })
+  });
   const handleScroll = () => {
     setState({
       x: window.scrollX,
-      y: window.scrollY.
-    })
-  }
+      y: window.scrollY,
+    });
+  };
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [])
+  }, []);
   return state;
-}
+};
