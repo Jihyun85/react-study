ced2ea7ccb5cf57c2e2c39b6a383a47788cc8958
Jihyun <hwiyu25@gmail.com>
2021-04-10 21:10:38 +0900

    벨로퍼트와 함께하는 모던 리액트 -  1장 7 useState(Counter) 추가

--- src/Counter.js
+++ src/Counter.js
@@ -3,8 +3,8 @@ import React, { useState } from "react";
 export default function Counter() {
   const [number, setNumber] = useState(0);
 
-  const plusNumber = () => setNumber((prev) => prev + 1);
-  const minusNumber = () => setNumber((prev) => prev - 1);
+  const plusNumber = () => setNumber(number + 1); // number + 1이라는 값을 새로 넣어줌
+  const minusNumber = () => setNumber((prev) => prev - 1); // 기존 값에서 1을 빼줌
 
   return (
     <div>
