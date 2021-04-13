c54d11b27b80d99e57f6001bda013d332ac66928
Jihyun <hwiyu25@gmail.com>
2021-04-13 16:35:41 +0900

    벨로퍼트와 함께하는 모던 리액트 -  4장 API 연동하기_1, 2

---
+++ src/User.js
@@ -0,0 +1,34 @@
+import React from "react";
+import axios from "axios";
+import useAsync from "./useAsync";
+
+async function getUsers() {
+  const response = await axios.get(
+    `https://jsonplaceholder.typicode.com/users`
+  );
+  return response.data;
+}
+
+function Users() {
+  const [state, refetch] = useAsync(getUsers(), [], true);
+  const { loading, error, data: users } = state;
+
+  if (loading) return <div>로딩중...</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!users) return <button onClick={refetch}>불러오기</button>;
+
+  return (
+    <>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={refetch}>다시 불러오기</button>
+    </>
+  );
+}
+
+export default Users;
