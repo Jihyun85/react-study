868cbe0d26b92ea9053ef5d7c246ed277e707ba3
Jihyun <hwiyu25@gmail.com>
2021-04-16 16:45:12 +0900

    벨로퍼트와 함께하는 모던 리액트 - 7장 리덕스 미들웨어_redux-saga

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route } from "react-router";
 import PostPage from "./pages/PostPage";
 import PostListPage from "./pages/PostListPage";
+import CounterContainer from "./containers/CounterContainer";
 
 function App() {
   return (
     <>
+      <CounterContainer />
       <Route path="/" component={PostListPage} exact={true} />
       <Route path="/:id" component={PostPage} />
     </>
--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import rootReducer from "./modules";
+import rootReducer, { rootSaga } from "./modules";
 import { applyMiddleware, createStore } from "redux";
 import logger from "redux-logger";
 import { composeWithDevTools } from "redux-devtools-extension";
 import ReduxThunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+  composeWithDevTools(applyMiddleware(ReduxThunk, sagaMiddleware, logger))
 );
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <React.StrictMode>
