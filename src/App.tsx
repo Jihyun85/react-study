74b739caa29ea1bfb147922a449b6c95f1ed7221
Jihyun <hwiyu25@gmail.com>
2021-04-17 13:20:20 +0900

    벨로퍼트와 함께하는 모던 리액트 - 8장 타입스크립트_Context API 활용

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import Counter from "./Counter";
 import Greetings from "./Greeting";
 import MyForm from "./myForm";
 import ReducerSample from "./ReducerSample";
+import { SampleProvider } from "./SampleContext";
 
 function App() {
   const onSubmit = (form: { name: string; description: string }) => {
     console.log(form);
   };
-  return <ReducerSample />;
+  return (
+    <SampleProvider>
+      <ReducerSample />
+    </SampleProvider>
+  );
 }
 
 export default App;
